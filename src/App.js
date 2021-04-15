e99c1c604e295504f14ec33a8f6afd1ff92f4028
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-04-16 00:11:19 +0100

    Laying the foundations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+class ItemList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            items: [
+                {
+                    id: 1,
+                    label: 'List item 1'
+                },
+                {
+                    id: 2,
+                    label: 'List item 2'
+                },
+                {
+                    id: 3,
+                    label: 'List item 3'
+                },
+                {
+                    id: 4,
+                    label: 'List item 4'
+                }
+            ],
+            hasErrored: false,
+            isLoading: false
+        };
+    }
+    render() {
+        if (this.state.hasErrored) {
+            return <p>Sorry! There was an error loading the items</p>;
+        }
+        if (this.state.isLoading) {
+            return <p>Loading…</p>;
+        }
+        return (
+            <ul>
+                {this.state.items.map((item) => (
+                    <li key={item.id}>
+                        {item.label}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
 }
-
-export default App;
+export default ItemList;
