860aa74d0ac9b8f737599e8528e35f9961fa657b
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-04-18 02:36:52 +0100

    old Context

--- src/App.js
+++ src/App.js
@@ -1,60 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React, { Component } from "react";
-const style = {
-  dark: {
-    backgroundColor: "black",
-    color: "white",
-  },
-  light: {
-    backgroundColor: "white",
-    color: "black",
-  },
-};
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      theme: "dark",
-    };
-    this.changeTheme = this.changeTheme.bind(this)
-  }
-  changeTheme(){
-    if(this.state.theme==="dark"){
-      this.setState({theme: "light"})
-    }else{
-      this.setState({theme: "dark"})
-    }
-  }
-  render() {
-    return (
-      <>
-        <button onClick={this.changeTheme}>
-          Theme : {this.state.theme}
-        </button>
-        <Toolbar theme={this.state.theme} />
-      </>
-    );
-  }
-}
+// Create a Context
+const { Provider, Consumer } = React.createContext();
+// It returns an object with 2 values:
+// { Provider, Consumer }
 
-function Toolbar(props) {
-  // The Toolbar component must take an extra "theme" prop
-  // and pass it to the ThemedButton. This can become painful
-  // if every single button in the app needs to know the theme
-  // because it would have to be passed through all components.
+function App() {
+  // Use the Provider to make a value available to all
+  // children and grandchildren
   return (
-    <div>
-      <ThemedButton theme={props.theme} />
-    </div>
+    <Provider value={43}>
+      <div>
+        <Display />
+      </div>
+    </Provider>
   );
 }
 
-class ThemedButton extends React.Component {
-  render() {
-    return <Button theme={this.props.theme} />;
-  }
+function Display() {
+  // Use the Consumer to grab the value from context
+  // Notice this component didn't get any props!
+  return <Consumer>{(value) => <div>The answer is {value}.</div>}</Consumer>;
 }
-
-var Button = ({ theme }) => <button style={style[theme]}>Click Me</button>;
 export default App;
