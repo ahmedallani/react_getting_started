1ff5d03443b881b9635fb9d167293e2e3e0ae04f
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-04-18 02:10:47 +0100

    exemple

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+const style = {
+  dark: {
+    backgroundColor: "black",
+    color: "white",
+  },
+  light: {
+    backgroundColor: "white",
+    color: "black",
+  },
+};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: "dark",
+    };
+    this.changeTheme = this.changeTheme.bind(this)
+  }
+  changeTheme(){
+    if(this.state.theme==="dark"){
+      this.setState({theme: "light"})
+    }else{
+      this.setState({theme: "dark"})
+    }
+  }
+  render() {
+    return (
+      <>
+        <button onClick={this.changeTheme}>
+          Theme : {this.state.theme}
+        </button>
+        <Toolbar theme={this.state.theme} />
+      </>
+    );
+  }
+}
 
-function App() {
+function Toolbar(props) {
+  // The Toolbar component must take an extra "theme" prop
+  // and pass it to the ThemedButton. This can become painful
+  // if every single button in the app needs to know the theme
+  // because it would have to be passed through all components.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemedButton theme={props.theme} />
     </div>
   );
 }
 
+class ThemedButton extends React.Component {
+  render() {
+    return <Button theme={this.props.theme} />;
+  }
+}
+
+var Button = ({ theme }) => <button style={style[theme]}>Click Me</button>;
 export default App;
